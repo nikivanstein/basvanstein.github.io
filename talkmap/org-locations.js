63f9657829b09298de31e6e8b2f724daef5a4352
Bas van Stein <steinbvan@liacs.leidenuniv.nl>
2023-10-27 14:15:23 +0000

    Talkmap populated

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,72 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Xiamen, China",
+    24.5438732,
+    118.0768065
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Budapest, Hungary",
+    47.4978918,
+    19.0401609
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Vancouver, Canada",
+    49.2608724,
+    -123.113952
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Santa Clara, California, United States",
+    37.2333253,
+    -121.6846349
+  ],
+  [
+    "Lisbon, Portugal",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "Leiden, The Netherlands",
+    52.1594747,
+    4.4908843
+  ],
+  [
+    "Eindhoven, The Netherlands",
+    51.4392648,
+    5.478633
+  ],
+  [
+    "Munich, Germany",
+    48.1371079,
+    11.5753822
+  ],
+  [
+    "Amsterdam, The Netherlands",
+    52.3730796,
+    4.8924534
+  ],
+  [
+    "IJmuiden, The Netherlands",
+    52.4574236,
+    4.6111981
+  ],
+  [
+    "Canberra, Australia",
+    -35.2975906,
+    149.1012676
+  ],
+  [
+    "Saint-Etienne, France",
+    45.4401467,
+    4.3873058
+  ],
+  [
+    "Cadiz, Spain",
+    36.5262463,
+    -5.9008367301110844
+  ],
+  [
+    "Washington DC, USA",
+    38.8950368,
+    -77.0365427
   ]
 ];
